e0546ff7f7ffd6b3874a9fea9466ceb2122e205c
Volodymyr <vovkul@gmail.com>
2023-10-30 23:29:47 +0100

    add functions add and remove fields

--- script.js
+++ script.js
@@ -6,3 +6,28 @@ const elements = {
 };
 
 const { form, formContainer, buttonAdd, buttonRemove } = elements;
+
+buttonAdd.addEventListener("click", handlerAddField);
+buttonRemove.addEventListener("click", handlerRemoveField);
+form.addEventListener("submit", handlerSearch);
+
+function handlerSearch(evt) {
+  evt.preventDefault();
+}
+
+function handlerAddField() {
+  formContainer.insertAdjacentHTML(
+    "beforeend",
+    `<input type="text" name="country"/>`
+  );
+}
+
+function handlerRemoveField() {
+  const { children, lastElementChild } = formContainer;
+
+  if (children.length === 1) {
+    return;
+  }
+
+  lastElementChild.remove();
+}

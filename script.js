17efb0016482f666c5dec455a522e2480d8032a9
Volodymyr <vovkul@gmail.com>
2023-10-30 16:59:15 +0100

    add styles+first step

---
+++ script.js
@@ -0,0 +1,8 @@
+const elements = {
+  form: document.querySelector(".js-search"),
+  formContainer: document.querySelector(".js-form-container"),
+  buttonAdd: document.querySelector(".js-add"),
+  buttonRemove: document.querySelector(".js-remove"),
+};
+
+const { form, formContainer, buttonAdd, buttonRemove } = elements;
